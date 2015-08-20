97e60de4415bc712b540ce454604bdd7ded08b52
Juho Vepsalainen <bebraw@gmail.com>
2015-08-20 14:15:59 +0300

    Set up initial table

--- app/app.jsx
+++ app/app.jsx
@@ -23,6 +23,27 @@ export default class App extends React.Component {
   renderReports(reports) {
     console.log(reports);
 
-    return <span>The data should go here.</span>;
+    return (
+      <table className='pure-table'>
+        <thead>
+          <tr>
+            <td>Date</td>
+            <td>Size</td>
+            <td>Hit total</td>
+            <td>Cache hit</td>
+            <td>Cache missed</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>date should go here</td>
+            <td>size should go here</td>
+            <td>hit total should go here</td>
+            <td>cache hit should go here</td>
+            <td>cache missed should go here</td>
+          </tr>
+        </tbody>
+      </table>
+    );
   }
 };

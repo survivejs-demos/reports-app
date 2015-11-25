032739560b7af9a0eaf6c23f9430743f832f44af
Juho Vepsalainen <bebraw@gmail.com>
2015-11-25 14:10:29 +0200

    Update to match boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
-'use strict';
 var path = require('path');
-
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var Clean = require('clean-webpack-plugin');
 var merge = require('webpack-merge');
 
-var TARGET = process.env.npm_lifecycle_event;
-var APP_DIR = path.resolve(__dirname, 'app');
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+  css: [
+    path.join(__dirname, 'app'),
+    path.join(__dirname, 'node_modules/purecss')
+  ]
+};
 
 var common = {
+  entry: PATHS.app,
   resolve: {
     extensions: [
       '', '.js', '.jsx', '.css'
@@ -18,68 +25,80 @@ var common = {
   module: {
     loaders: [
       {
-        test: /\.css$/,
-        loaders: ['style', 'css']
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: [PATHS.app]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Reports application'
+    })
+  ]
 };
 
-if (TARGET === 'start') {
-  var IP = 'localhost'; // '0.0.0.0'; for wlan (not on win!)
-  var PORT = 3000;
-
+if(TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
-    ip: IP,
-    port: PORT,
     devtool: 'eval-source-map',
-    entry: [
-      'webpack-dev-server/client?http://' + IP + ':' + PORT,
-      'webpack/hot/dev-server',
-      APP_DIR
-    ],
-    output: {
-      path: __dirname,
-      filename: 'bundle.js'
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // display only errors to reduce the amount of output
+      stats: 'errors-only',
+
+      // parse host and port from env so this is easy
+      // to customize
+      host: process.env.HOST,
+      port: process.env.PORT
     },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env': {
-          'NODE_ENV': JSON.stringify('development'),
-        }
-      }),
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NoErrorsPlugin(),
-      new HtmlWebpackPlugin()
-    ],
     module: {
       loaders: [
         {
-          test: /\.jsx?$/,
-          loaders: ['react-hot', 'babel'],
-          include: [APP_DIR]
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: PATHS.css
         }
       ]
-    }
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('development')
+      }),
+      new webpack.HotModuleReplacementPlugin()
+    ]
   });
 }
-else {
+
+if(TARGET === 'build') {
   module.exports = merge(common, {
     entry: {
-      app: APP_DIR,
+      app: PATHS.app,
       vendors: ['react']
     },
     output: {
-      path: './gh-pages',
-      filename: 'bundle.[chunkhash].js',
+      path: PATHS.build,
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[chunkhash].js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: PATHS.css
+        }
+      ]
     },
     plugins: [
-      new Clean(['gh-pages']),
+      new Clean([PATHS.build]),
+      new ExtractTextPlugin('styles.[chunkhash].css'),
       new webpack.DefinePlugin({
-        'process.env': {
-          // This has effect on the react lib size
-          'NODE_ENV': JSON.stringify('production'),
-        }
+        // This has effect on the react lib size
+        'process.env.NODE_ENV': JSON.stringify('production')
       }),
       new webpack.optimize.DedupePlugin(),
       new webpack.optimize.UglifyJsPlugin({
@@ -87,19 +106,9 @@ else {
           warnings: false
         }
       }),
-      new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.[chunkhash].js'),
-      new HtmlWebpackPlugin({
-        title: 'Reports application'
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendors', 'manifest']
       })
-    ],
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?$/,
-          loaders: ['babel'],
-          include: [APP_DIR],
-        }
-      ]
-    }
+    ]
   });
 }
